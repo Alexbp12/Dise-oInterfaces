461c4ab7121b86dfa83c5cca550ab122a2259c6a
Alexbp <alexbp200612@gmail.com>
2025-08-26 18:44:13 -0500

    React

---
+++ my-react-app/my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+// Define la función App() para que retorne el JSX de la tarjeta.
+function App() {
+  return (
+    <div className="card">
+      <h1>Alexander Botello</h1>
+      <p>Ingeniero de sistemas</p>
+    </div>
+    
+  );
+}
+
+export default App;
